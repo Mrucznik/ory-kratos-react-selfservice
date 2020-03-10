57c01eef0a583e66a3eaf2e034655cc0b8c66dc7
Mrucznik <mrucznix@gmail.com>
2020-03-10 22:34:21 +0100

    Login & registration beginning

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,20 +2,19 @@ import React, {useEffect, useState} from 'react';
 import IconLogo from "../components/icons/IconLogo";
 import FormFields from "../components/FormFields";
 import FormErrors from "../components/FormErrors";
-import {Link, Redirect, useLocation} from "react-router-dom";
+import {Link} from "react-router-dom";
 import config from "../config";
+import useQuery from "../util/userQuery";
+import {sortFormFields} from "../util/translations";
 
 type LoginProps = {
     formAction: string
 }
 
-function useQuery() {
-    return new URLSearchParams(useLocation().search);
-}
 
 const Login = (props: LoginProps) => {
     const query = useQuery();
-    const [formFields, setFormFields] = useState({ methods: {password: {config: {action: '', fields: []}}}});
+    const [response, setResponse] = useState({ methods: {password: {config: {errors: [], action: '', fields: []}}}});
     const request = query.get('request');
 
     //todo: redirect to /.ory/kratos/public/self-service/browser/flows/login on empty request
@@ -23,7 +22,7 @@ const Login = (props: LoginProps) => {
     useEffect(() => {
         fetch(`${config.kratos.public}/self-service/browser/flows/requests/login?request=${request}`)
             .then(res => res.json())
-            .then((data) => setFormFields(data))
+            .then((data) => setResponse(data))
             .catch(console.log);
     }, []);
 
@@ -33,10 +32,10 @@ const Login = (props: LoginProps) => {
                 <IconLogo/>
                 <h5 className="subheading">Welcome to this example login screen!</h5>
 
-                <FormErrors className="overview-form-errors" errors={[]}/>
+                <FormErrors className="overview-form-errors" errors={response.methods.password.config.errors || []}/>
 
-                <form action={formFields.methods.password.config.action} method="POST">
-                    <FormFields formFields={formFields.methods.password.config.fields}/>
+                <form action={response.methods.password.config.action} method="POST">
+                    <FormFields formFields={response.methods.password.config.fields}/>
                     <button type="submit">Sign In</button>
                 </form>
 
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import IconLogo from "../components/icons/IconLogo";
 import FormFields from "../components/FormFields";
 import FormErrors from "../components/FormErrors";
 import {Link} from "react-router-dom";
+import config from "../config";
+import useQuery from "../util/userQuery";
 
 type RegistrationProps = {
     formAction: string
 }
 
 const Registration = (props: RegistrationProps) => {
+    const query = useQuery();
+    const [response, setResponse] = useState({ methods: {password: {config: {errors: [], action: '', fields: []}}}});
+    const request = query.get('request');
+
+    //todo: redirect to /.ory/kratos/public/self-service/browser/flows/login on empty request
+
+    useEffect(() => {
+        fetch(`${config.kratos.public}/self-service/browser/flows/requests/registration?request=${request}`)
+            .then(res => res.json())
+            .then((data) => setResponse(data))
+            .catch(console.log);
+    }, []);
+
     return (
         <div className="auth">
             <div className="container">
@@ -16,10 +31,10 @@ const Registration = (props: RegistrationProps) => {
                 <h5 className="subheading">Welcome to SecureApp! <br/>
                     Use the form below to sign up:</h5>
 
-                <FormErrors className="overview-form-errors" errors={[]}/>
+                <FormErrors className="overview-form-errors" errors={response.methods.password.config.errors || []}/>
 
-                <form action={props.formAction} method="POST">
-                    <FormFields formFields={[]}/>
+                <form action={response.methods.password.config.action} method="POST">
+                    <FormFields formFields={response.methods.password.config.fields}/>
                     <button type="submit">Sign Up</button>
                 </form>
 
