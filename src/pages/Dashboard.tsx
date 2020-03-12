0c291b1fbb84918a606e6ebd9d38a47b83ae2289
Mrucznik <mrucznix@gmail.com>
2020-03-12 16:36:15 +0100

    .

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../components/Header";
+import config from "../config";
+import {Identity, Session} from "@oryd/kratos-client";
 
 const Dashboard = () => {
+    const s = new Session();
+    s.identity = new Identity();
+    s.identity.traits = [];
+    const [session, setSession] = useState(s);
+    const [headers, setHeaders] = useState({});
+    const [token, setToken] = useState({raw:'', claims: ''});
+
+    useEffect(() => {
+        fetch(`${config.kratos.public}/sessions/whoami`)
+            .then(res => {
+                if (!res.ok) throw res;
+                setHeaders(res.headers);
+                return res.json();
+            })
+            .then((data) => setSession(data))
+            .catch((reason => {
+                console.log(reason)
+            }));
+    }, []);
+
     return (
         <div className="content">
             <Header/>
             <div className="container">
-                <h4 className="greeting">Welcome back, <span
-                    className="user-identifier">{/*session.identity.traits.email*/}</span>!</h4>
+                <h4 className="greeting">Welcome back, <span className="user-identifier">
+                    {
+                        // @ts-ignore
+                        session.identity.traits.email
+                    }
+                </span>!</h4>
                 <h5>This example app is secure by default</h5>
                 <p>
                     Hello, nice to have you! You signed up with this data:
                 </p>
-                <pre><code>{/*jsonPretty session.identity.traits*/}</code></pre>
+                <pre><code>{JSON.stringify(session.identity.traits, null, 2)}</code></pre>
                 <p>
                     Requests passing to your application (and this example) flow
                     through our proxy which you install using the <a href="/">ORY CLI</a>.
                 </p>
-                <p>
-                    The proxy ensures that any request attempting to access
-                    protected URLs, resources, ... is authorized to do so. When that's the case,
-                    the headers coming to your app will include the HTTP <code>Authorization</code> Header:
-                </p>
-                <pre><code>{/*headers*/}</code></pre>
-
-                <h2>Session without effort</h2>
-                <p>
-                    The token in that HTTP Authorization Header is a <a href="https://jwt.io/" target="_blank" rel="noopener noreferrer">JSON Web
-                    Token</a>,
-                    cryptographically signed by ORY. Once decoded, the session information is revealed. No REST requests
-                    needed.
-                    No SDKs required. Simple, fast, and secure.
-                </p>
-                <p>The raw JSON Web Token included in the header is:</p>
-                <pre><code>{/*token.raw*/}</code></pre>
-                <p>The parsed representation is:</p>
-                <pre><code>{/*jsonPretty token.claims*/}</code></pre>
             </div>
         </div>
     );
--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -5,7 +5,7 @@ import KratosForm from "../components/KratosForm";
 import {useQuery} from "../util/UseQuery";
 import config from "../config";
 import RedirectUrl from "../components/RedirectURL";
-import {VerificationRequest} from "@oryd/kratos-client/model/verificationRequest";
+import {VerificationRequest} from "@oryd/kratos-client/dist/model/verificationRequest";
 
 const Verify = () => {
     const query = useQuery();
@@ -14,7 +14,7 @@ const Verify = () => {
 
     useEffect(() => {
         if(request !== null) {
-            fetch(`${config.kratos.public}/self-service/browser/flows/requests/login?request=${request}`)
+            fetch(`${config.kratos.public}/self-service/browser/flows/requests/verification?request=${request}`)
                 .then(res => {
                     if (!res.ok) throw res;
                     return res.json();
@@ -25,7 +25,7 @@ const Verify = () => {
     }, [request]);
 
     if(request === null) {
-        return <RedirectUrl to={`${config.kratos.browser}/self-service/browser/flows/verification/email`}/>
+        return <RedirectUrl to={`${config.kratos.public}/self-service/browser/flows/verification/init/email`}/>
     } else {
         return (
             <div className="content">
@@ -35,7 +35,7 @@ const Verify = () => {
                         <div className="container">
                             <h4>Resend verification code</h4>
 
-                            <FormErrors className="overview-form-errors" errors={[]}/>
+                            <FormErrors className="overview-form-errors" errors={response.form?.errors}/>
 
                             <form action={response.form.action} method={response.form.method}>
                                 {
